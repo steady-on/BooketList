6e983b44296ecdca0efe236c7ac341cf63caa4f3
steady-on <roen.steady.on@gmail.com>
2023-09-26 21:42:14 +0900

    refactor(AladinAPIManager): 열거형에서 구조체로 타입변경 #1
    - static 메서드 선언으로 굳이 메모리에 계속 올라와 있을 필요가 없으므로 타입을 구조체로 변경하고 메서드도 인스턴스 메서드로 변경
    - 그 외 폴더 정리

--- BookitList/BookitList/Network/AladinAPI/AladinAPIManager.swift
+++ BookitList/BookitList/Network/AladinAPI/AladinAPIManager.swift
@@ -8,8 +8,8 @@
 import Foundation
 import Alamofire
 
-enum AladinAPIManager<T: Decodable> {
-    static func request(type: T.Type, api: AladinRouter, completion: @escaping (Result<T, Error>) -> Void) {
+struct AladinAPIManager<T: Decodable> {
+    func request(type: T.Type, api: AladinRouter, completion: @escaping (Result<T, Error>) -> Void) {
         AF.request(api).responseDecodable(of: T.self) { response in
             switch response.result {
             case .success(let data):
