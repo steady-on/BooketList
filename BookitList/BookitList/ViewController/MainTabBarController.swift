cb8f382642e34d5afff95ef5ba2ff49d6b886359
steady-on <roen.steady.on@gmail.com>
2023-09-28 07:59:31 +0900

    design: TabBar settings & entry point settings #8

--- BookitList/BookitList/ViewController/MainTabBarController.swift
+++ BookitList/BookitList/ViewController/MainTabBarController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
+    private let readingRecordViewController = ReadingRecordViewController()
+    private let bookShelfViewController = BookShelfViewController()
+    private let noteViewController = NoteViewController()
+    private let settingsViewController = SettingsViewController()
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        configureTabBar()
     }
     
+    func configureTabBar() {
+        tabBar.tintColor = .accent
 
-
-
+        readingRecordViewController.tabBarItem.image = UIImage(systemName: "house.fill")
+        bookShelfViewController.tabBarItem.image = UIImage(systemName: "books.vertical.fill")
+        noteViewController.tabBarItem.image = UIImage(systemName: "list.bullet.rectangle.fill")
+        settingsViewController.tabBarItem.image = UIImage(systemName: "gearshape.fill")
+        
+        let viewControllers = [readingRecordViewController, bookShelfViewController, noteViewController, settingsViewController].map { viewController in
+            UINavigationController(rootViewController: viewController)
+        }
+        
+        setViewControllers(viewControllers, animated: true)
+    }
 }
