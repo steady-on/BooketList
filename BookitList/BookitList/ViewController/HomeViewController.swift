fef0be2b581dbd7a069d235aba50790a2517a2bd
steady-on <roen.steady.on@gmail.com>
2023-09-28 07:44:50 +0900

    add: MainTabBarController 및 각 Tab에 들어갈 ViewController 생성 #8

---
+++ BookitList/BookitList/ViewController/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/09/28.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureHiararchy() {
+        super.configureHiararchy()
+    }
+}
---
+++ BookitList/BookitList/ViewController/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/09/28.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
