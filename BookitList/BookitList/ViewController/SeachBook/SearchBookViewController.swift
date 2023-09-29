a8062be918f42d49b7d391abc9dff6647b233b8f
steady-on <roen.steady.on@gmail.com>
2023-09-29 19:59:44 +0900

    design(SearchBookViewController): navigationBar에 searchController 추가 #10

--- BookitList/BookitList/ViewController/SeachBook/SearchBookViewController.swift
+++ BookitList/BookitList/ViewController/SeachBook/SearchBookViewController.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 
-class SearchBookViewController: BaseViewController {
+final class SearchBookViewController: BaseViewController {
+    
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "책제목, ISBN, 작가이름..."
+        searchController.searchBar.searchTextField.clearButtonMode = .always
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.returnKeyType = .search
+        return searchController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func configureHiararchy() {
+        super.configureHiararchy()
+        
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        title = "도서 검색"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
 }
