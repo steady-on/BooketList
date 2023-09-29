f7ec80c3f1423d7dd24d3a3071c4b0bf2f6f05e7
steady-on <roen.steady.on@gmail.com>
2023-09-29 19:27:09 +0900

    feat(ReadingRecordViewController): navigationBar에 검색View로 이어지는 버튼 생성 #10
    SearchBookViewController에서는 tabBar가 보이지 않도록 함

--- BookitList/BookitList/ViewController/ReadingRecordViewController.swift
+++ BookitList/BookitList/ViewController/ReadingRecordViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 final class ReadingRecordViewController: BaseViewController {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
     override func configureHiararchy() {
         super.configureHiararchy()
+        
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(pushSearchBookView))
+    }
+
+    @objc private func pushSearchBookView() {
+        let searchBookView = SearchBookViewController()
+        searchBookView.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(searchBookView, animated: true)
     }
 }
---
+++ BookitList/BookitList/ViewController/SeachBook/SearchBookViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchBookViewController.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/09/29.
+//
+
+import UIKit
+
+class SearchBookViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
