0d9bd639b0f062e36efc9ca9fe717581f2e26753
steady-on <roen.steady.on@gmail.com>
2023-10-02 21:20:02 +0900

    fix(BLBookmarkImageView): draw 메서드가 아니라 생성자에서 configureHiararchy를 설정하도록 함 #10

--- BookitList/BookitList/Product/BLBookmarkImageView.swift
+++ BookitList/BookitList/Product/BLBookmarkImageView.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class BLBookmarkImageView: UIImageView {
-    
-    override func draw(_ rect: CGRect) {
+    init() {
+        super.init(frame: .zero)
         configureHiararchy()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func configureHiararchy() {
@@ -18,7 +23,7 @@ class BLBookmarkImageView: UIImageView {
         let image = UIImage(systemName: "bookmark.fill", withConfiguration: symbolConfig)
         
         self.image = image
-        tintColor = .highlight
+        tintColor = .accent
         contentMode = .scaleToFill
     }
 }
