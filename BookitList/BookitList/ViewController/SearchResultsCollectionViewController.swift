b9222f21947cd76ee062a4c624d76d0e2df63d97
steady-on <roen.steady.on@gmail.com>
2023-10-02 12:49:38 +0900

    add: SearchResultsCollectionViewController 구현 #10
    - 원래는 알라딘 검색 결과를 보여주기 위해 만들었으나, UISearchController의 searchResultsController는 입력과 동시에 검색이나 필터링이 이루어지도록 되어 있어, 추후 BookShelfViewController에서 사용하기 위해 구현만 함.

---
+++ BookitList/BookitList/ViewController/SearchResultsCollectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchResultsCollectionViewController.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/10/02.
+//
+
+import UIKit
+
+final class SearchResultsCollectionViewController: UICollectionViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureDataSource()
+    }
+    
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<BLSearchResultCollectionViewCell, Item> { cell, indexPath, itemIdentifier in
+            cell.item = itemIdentifier
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: self.collectionView) { collectionView, indexPath, itemIdentifier -> UICollectionViewCell in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    private func updateSnapshot(for newItems: [Item]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(newItems)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
