2944901e409a6df0c097134e8e933122f0f1c384
steady-on <roen.steady.on@gmail.com>
2023-10-02 13:12:32 +0900

    design(SearchBookViewController): 검색결과를 보여줄 collectionView를 만들고 layout과 dataSource 정의 #10

--- BookitList/BookitList/ViewController/SeachBook/SearchBookViewController.swift
+++ BookitList/BookitList/ViewController/SeachBook/SearchBookViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 final class SearchBookViewController: BaseViewController {
     
+    enum Section {
+        case main
+    }
+    
     private let searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "책제목, ISBN, 작가이름..."
@@ -20,6 +24,9 @@ final class SearchBookViewController: BaseViewController {
     
     private let placeholderView = BLDirectionView(symbolName: "magnifyingglass", direction: "검색어를 입력해주세요.")
 
+    private var searchResultsCollectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,7 +38,11 @@ final class SearchBookViewController: BaseViewController {
         
         configureNavigationBar()
         
-        view.addSubview(placeholderView)
+        searchResultsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCollectionViewLayout())
+        configureDataSource()
+        
+        let components = [placeholderView, searchResultsCollectionView]
+        components.forEach { component in view.addSubview(component!) }
     }
     
     private func configureNavigationBar() {
@@ -46,5 +57,33 @@ final class SearchBookViewController: BaseViewController {
             make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.5)
         }
+        
+        searchResultsCollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func createCollectionViewLayout() -> UICollectionViewLayout {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        configuration.showsSeparators = false
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        return layout
+    }
+    
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<BLSearchResultCollectionViewCell, Item> { cell, indexPath, itemIdentifier in
+            cell.item = itemIdentifier
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: searchResultsCollectionView) { collectionView, indexPath, itemIdentifier -> UICollectionViewCell in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    private func updateSnapshot(for newItems: [Item]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(newItems)
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
 }
