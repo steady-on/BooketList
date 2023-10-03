fc8562f82caacaaf6e3a1d2fde9c78c1359c2728
steady-on <roen.steady.on@gmail.com>
2023-10-03 18:13:39 +0900

    update(SearchBookViewModel): prefetching을 위한 메서드 requestNextPage 추가 및 관련 프로퍼티 추가 #10
    - 검색 직후 똑같은 키워드로는 요청이 되지 않도록 함
    - 새로운 키워드로 검색 시 페이지 관련 프로퍼티를 초기값으로 변경
    - 전체 검색 결과 개수를 저장하는 프로퍼티 추가: 알라딘에 하나의 검색어로 요청가능한 최대 개수는 200개이므로 200미만인 경우만 저장
    - 다음 페이지를 fetching하는 requestNextPage 메서드 작성
    - 현재 페이지에 1을 더한 값이 maxPage 보다 크면 요청이 되지 않도록 함
    - KingFisher의 ImagePrefetcher를 활용하여 이미지를 표시하기 전에 다운로드하여 사용자 경험을 좋게 함

--- BookitList/BookitList/ViewController/SeachBook/SearchBookViewModel.swift
+++ BookitList/BookitList/ViewController/SeachBook/SearchBookViewModel.swift
@@ -6,15 +6,52 @@
 //
 
 import Foundation
+import Kingfisher
 
 final class SearchBookViewModel {
-    let searchResultItems: Observable<[Item]> = Observable([])
     
-    func search(for keyword: String) {
-        AladinAPIManager().request(type: AladinSearchResponse.self, api: .itemSearch(query: keyword, isEbook: false, page: 1)) { result in
+    private var currentPage = 1
+    private var totalResults = AladinConstant.maximumResultCount
+    private var maxPage: Int {
+        let portion = totalResults / AladinConstant.maxResultCount
+        return portion + ((totalResults % AladinConstant.maxResultCount == 0) ? 0 : 1)
+    }
+
+    private var keyword: String = ""
+    
+    let searchResultItems: Observable<[Item]> = Observable([])
+    var resultItemCount: Int { searchResultItems.value.count }
+    
+    func requestSearchResult(for newKeyword: String) {
+        guard newKeyword != keyword else { return }
+        
+        keyword = newKeyword
+        currentPage = 1
+        totalResults = AladinConstant.maximumResultCount
+        
+        AladinAPIManager().request(type: AladinSearchResponse.self, api: .itemSearch(query: keyword, isEbook: false, page: currentPage)) { result in
             switch result {
             case .success(let data):
+                if data.totalResults < AladinConstant.maxResultCount {
+                    self.totalResults = data.totalResults
+                }
                 self.searchResultItems.value = data.item
+            case .failure(let failure):
+                dump(failure)
+            }
+        }
+    }
+    
+    func requestNextPage() {
+        guard currentPage + 1 <= maxPage else { return }
+        currentPage += 1
+
+        AladinAPIManager().request(type: AladinSearchResponse.self, api: .itemSearch(query: keyword, isEbook: false, page: currentPage)) { result in
+            switch result {
+            case .success(let data):
+                self.searchResultItems.value.append(contentsOf: data.item)
+                let coverURls = data.item.compactMap { URL(string: $0.cover) }
+                ImagePrefetcher(urls: coverURls).start()
             case .failure(let failure):
                 dump(failure)
             }
