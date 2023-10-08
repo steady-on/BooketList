a9694e8cc5faa3fd84281d4d0b3c2ebbc1dd63bc
steady-on <roen.steady.on@gmail.com>
2023-10-09 00:14:27 +0900

    update(BookType): button 그룹을 만들기 위한 수정 #13
    - buttonTitle 프로퍼티 추가
    - 반복문 사용을 위한 CaseIterable 프로토콜 추가

--- BookitList/BookitList/Model/BookType.swift
+++ BookitList/BookitList/Model/BookType.swift
@@ -8,8 +8,16 @@
 import Foundation
 import RealmSwift
 
-enum BookType: Int, PersistableEnum {
+enum BookType: Int, CaseIterable, PersistableEnum {
     case paper
     case ebook
     case audio
+    
+    var buttonTitle: String {
+        switch self {
+        case .paper: return "종이책"
+        case .ebook: return "전자책"
+        case .audio: return "오디오북"
+        }
+    }
 }
