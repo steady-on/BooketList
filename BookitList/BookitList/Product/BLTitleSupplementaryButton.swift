1dd7465b111ddf5fd128e5c9cce0854ef6bb304d
steady-on <roen.steady.on@gmail.com>
2023-10-23 03:13:11 +0900

    add: CollectionView 위의 title 역할을 할 button 정의 #15

---
+++ BookitList/BookitList/Product/BLTitleSupplementaryButton.swift
@@ -0,0 +1,39 @@
+//
+//  BLTitleSupplementaryButton.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/10/23.
+//
+
+import UIKit
+
+class BLTitleSupplementaryButton: UIButton {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        contentHorizontalAlignment = .leading
+        
+        var titleContainer = AttributeContainer()
+        titleContainer.font = .preferredFont(forTextStyle: .title2)
+        titleContainer.foregroundColor = .label
+        
+        var config = UIButton.Configuration.plain()
+        config.buttonSize = .large
+        config.image = UIImage(systemName: "chevron.down")
+        config.imagePlacement = .trailing
+        config.imagePadding = 8
+        config.attributedTitle = AttributedString(title, attributes: titleContainer)
+        configuration = config
+    }
+}
