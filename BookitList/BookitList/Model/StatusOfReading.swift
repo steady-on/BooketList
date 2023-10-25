aeba75dd656517842899e1f2f3bc42c7bd54010f
steady-on <roen.steady.on@gmail.com>
2023-10-25 17:58:29 +0900

    update(StatusOfReading): view에 나타낼 label 프로퍼티 정의 #15

--- BookitList/BookitList/Model/StatusOfReading.swift
+++ BookitList/BookitList/Model/StatusOfReading.swift
@@ -14,4 +14,14 @@ enum StatusOfReading: Int, PersistableEnum {
     case finished
     case pause
     case stop
+    
+    var label: String {
+        switch self {
+        case .notYet: return "아직 안 읽음"
+        case .reading: return "지금 읽는 중"
+        case .finished: return "다 읽음"
+        case .pause: return "잠시 읽기 중단 중"
+        case .stop: return "읽기 중단"
+        }
+    }
 }
