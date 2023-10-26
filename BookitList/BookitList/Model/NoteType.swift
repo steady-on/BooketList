b18d202d6279a4f954300abcd10b0af455cd33ee
steady-on <roen.steady.on@gmail.com>
2023-10-26 17:23:18 +0900

    update: NoteType에 따른 View 구현을 위해 Note Object에서 분리 및 CaseIterable 추가, title 프로퍼티 추가 #15

---
+++ BookitList/BookitList/Model/NoteType.swift
@@ -0,0 +1,29 @@
+//
+//  NoteType.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/10/26.
+//
+
+import Foundation
+import RealmSwift
+
+enum NoteType: Int, CaseIterable, PersistableEnum {
+    case quote
+    case summary
+    case thinking
+    case question
+    case report
+    case memory
+    
+    var title: String {
+        switch self {
+        case .quote: return "책속의 한줄"
+        case .summary: return "요약"
+        case .thinking: return "생각"
+        case .question: return "의문/질문"
+        case .report: return "감상"
+        case .memory: return "추억"
+        }
+    }
+}
