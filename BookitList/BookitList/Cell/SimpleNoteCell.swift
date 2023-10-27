fcfa1cfb7989c332b93c70593a84c92eff8dc3c9
steady-on <roen.steady.on@gmail.com>
2023-10-27 20:13:23 +0900

    add: AllRecordsForBookView에서 나타낼 SimpleNoteCell 구현 #15

---
+++ BookitList/BookitList/Cell/SimpleNoteCell.swift
@@ -0,0 +1,94 @@
+//
+//  SimpleNoteCell.swift
+//  BookitList
+//
+//  Created by Roen White on 2023/10/26.
+//
+
+import UIKit
+
+class SimpleNoteCell: BaseTableViewCell, ReuseIdentifier {
+    var note: Note! {
+        didSet {
+            typeLabel.setNoteType(to: note.type)
+            
+            if let page = note.page {
+                pageLabel.text = "\(page)쪽"
+            }
+            
+            createdAtLabel.text = note.createdAt.basicString
+            noteContentTextView.text = note.content
+        }
+    }
+    
+    private let backdropView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    private let typeLabel = BLNoteTypeLabel(noteType: .quote)
+    
+    private let pageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .callout)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let createdAtLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .callout)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let noteContentTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.backgroundColor = .clear
+        textView.textColor = .label
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        textView.isSelectable = false
+        return textView
+    }()
+    
+    override func configureHiararchy() {
+        backgroundColor = .clear
+        addSubview(backdropView)
+        
+        let components = [typeLabel, pageLabel, createdAtLabel, noteContentTextView]
+        components.forEach { component in
+            backdropView.addSubview(component)
+        }
+    }
+    
+    override func setConstraints() {
+        backdropView.snp.makeConstraints { make in
+            make.edges.equalTo(layoutMarginsGuide)
+        }
+        
+        typeLabel.snp.makeConstraints { make in
+            make.top.leading.equalTo(backdropView.layoutMarginsGuide)
+        }
+        
+        pageLabel.snp.makeConstraints { make in
+            make.leading.equalTo(typeLabel.snp.trailing).offset(8)
+            make.centerY.equalTo(typeLabel)
+        }
+        
+        createdAtLabel.snp.makeConstraints { make in
+            make.top.trailing.equalTo(backdropView.layoutMarginsGuide)
+            make.centerY.equalTo(typeLabel)
+        }
+        
+        noteContentTextView.snp.makeConstraints { make in
+            make.top.equalTo(typeLabel.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(backdropView.layoutMarginsGuide)
+            make.bottom.equalTo(backdropView.layoutMarginsGuide)
+        }
+    }
+}
